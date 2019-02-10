85e01bbc123e3419af3d3aaeb2df935353349ae6
Martin Straßer <martin.straszer@outlook.at>
2019-02-10 15:18:25 +0100

    Bugfix resource bundle

--- lib/scxml/src/main/java/at/jku/dke/inga/scxml/actions/DisplayValues.java
+++ lib/scxml/src/main/java/at/jku/dke/inga/scxml/actions/DisplayValues.java
@@ -1,10 +1,16 @@
 package at.jku.dke.inga.scxml.actions;
 
+import at.jku.dke.inga.rules.models.ResolveValuesDataModel;
+import at.jku.dke.inga.rules.services.ValuesResolver;
+import at.jku.dke.inga.shared.display.Display;
 import at.jku.dke.inga.shared.models.NonComparativeAnalysisSituation;
 import org.apache.commons.scxml2.ActionExecutionContext;
 import org.apache.commons.scxml2.SCXMLExpressionException;
 import org.apache.commons.scxml2.model.ModelException;
 
+/**
+ * Action that determines the values to display.
+ */
 public class DisplayValues extends BaseAction {
 
     /**
@@ -22,7 +28,13 @@ public class DisplayValues extends BaseAction {
         NonComparativeAnalysisSituation as = (NonComparativeAnalysisSituation) getContext(ctx).getAnalysisSituation();
 
         // Resolve Data
-
+        ResolveValuesDataModel model = new ResolveValuesDataModel(
+                getCurrentState(),
+                as,
+                getContext(ctx).getLocale(),
+                operation
+        );
+        Display display = new ValuesResolver().resolveValues(model);
 
         // Send to display
         // TODO
