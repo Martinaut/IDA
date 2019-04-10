cae4ffc1a6ba6818807966eea373550ac68d6836
Martin Straßer <martin.straszer@outlook.at>
2019-04-10 10:59:55 +0200

    CSP: Only one GL per dimension

--- app/ruleset_default/src/main/java/at/jku/dke/inga/app/ruleset/csp/RuleHelpers.java
+++ app/ruleset_default/src/main/java/at/jku/dke/inga/app/ruleset/csp/RuleHelpers.java
@@ -1,7 +1,14 @@
 package at.jku.dke.inga.app.ruleset.csp;
 
 import at.jku.dke.inga.app.ruleset.csp.domain.AnalysisSituationElement;
+import at.jku.dke.inga.data.models.DimensionSimilarity;
 
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+/**
+ * Contains helpers that are used in the CSP-rules files.
+ */
 public final class RuleHelpers {
     /**
      * Prevents creation of instances of this class.
@@ -9,11 +16,42 @@ public final class RuleHelpers {
     private RuleHelpers() {
     }
 
-    public static boolean allInSameCube(String cube, AnalysisSituationElement elem) {
+    /**
+     * Returns whether all elements are in the specified cube.
+     * <p>
+     * If {@code elem} is {@code null} or empty, {@code true} will be returned.
+     *
+     * @param cube The cube.
+     * @param elem The cube elements to check.
+     * @return {@code true} if all elements are in the specified cube; {@code false} otherwise.
+     */
+    public static boolean allInCube(String cube, AnalysisSituationElement elem) {
         if (elem == null) return true;
         if (elem.getElements().isEmpty()) return true;
 
         return elem.getElements().stream()
                 .allMatch(x -> x.getCube().equals(cube));
+    }
+
+    /**
+     * Returns whether there is only at maximum one element per dimension.
+     * <p>
+     * If {@code elem} is {@code null}, empty or contains no {@link DimensionSimilarity} objects, {@code true} will be returned.
+     * If {@code elem} contains elements which are no instances of {@link DimensionSimilarity}, {@code false} will be returned.
+     *
+     * @param elem The elements.
+     * @return {@code true} if there is maximum one element per dimension; {@code false} otherwise.
+     */
+    public static boolean maximumOneElementPerDimension(AnalysisSituationElement elem) {
+        if (elem == null) return true;
+        if (elem.getElements().isEmpty()) return true;
+        if (elem.getElements().stream().noneMatch(x -> x instanceof DimensionSimilarity)) return true;
+        if (elem.getElements().stream().anyMatch(x -> !(x instanceof DimensionSimilarity))) return false;
+
+        return elem.getElements().stream()
+                .map(x -> ((DimensionSimilarity) x).getDimension())
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .values().stream()
+                .allMatch(x -> x <= 1);
     }
 }
