a66b5c819e8b552adc79651fd629db8e8e12cbd0
Martin Straßer <martin.straszer@outlook.at>
2019-08-26 15:28:34 +0200

    Use drools for switching between as of comp as

---
+++ engine/rules/src/main/java/at/jku/dke/ida/rules/interfaces/SwitchAnalysisSituationServiceModel.java
@@ -0,0 +1,7 @@
+package at.jku.dke.ida.rules.interfaces;
+
+/**
+ * Model used by {@link at.jku.dke.ida.rules.services.SwitchAnalysisSituationService}.
+ */
+public interface SwitchAnalysisSituationServiceModel extends ServiceModel {
+}
