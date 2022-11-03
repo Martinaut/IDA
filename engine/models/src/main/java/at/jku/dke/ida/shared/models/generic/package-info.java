95a81e53fbd6a9499c2dc9d507838c5a4086bfe7
Martin Straßer <martin@straszer.eu>
2022-11-03 14:29:01 +0100

    Moved models to separate project

--- engine/models/src/main/java/at/jku/dke/ida/shared/models/generic/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * This package contains the definition of generic analysis situations.
- */
-package at.jku.dke.ida.shared.models.generic;
--- engine/models/src/main/java/at/jku/dke/ida/shared/models/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * This package contains the definition of analysis situations.
- */
-package at.jku.dke.ida.shared.models;
